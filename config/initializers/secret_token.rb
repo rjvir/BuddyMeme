ff0edb813d9c19aef4fadbc6c61896b079824122
Chris Wendel <chriwend@umich.edu>
2012-03-23 22:44:18 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuddyMeme::Application.config.secret_token = '1387830ab893069b7086583741f63cf016d2f919331cc3819dc9b9cb47e96dcfac4888de48d0ee099740502b100a7c48cbe1d4478ff24f5e16789760aba84e99'
