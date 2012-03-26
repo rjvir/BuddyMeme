431ae2e639f9d636ead737220b4e75e80efd6699
Chris Wendel <chriwend@umich.edu>
2012-03-26 02:27:40 -0400

    Callback

--- config/routes.rb
+++ config/routes.rb
@@ -51,9 +51,11 @@ BuddyMeme::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'home#index'
 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match 'home/callback' => 'home#callback'
 end
