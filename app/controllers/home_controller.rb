59699207e36fe4e27404c27442a754cf1f9798bf
Chris Wendel <chriwend@umich.edu>
2012-03-26 01:49:01 -0400

    eh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+	protect_from_forgery
   def index
+  		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
+		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream") 	
+		puts session.to_s + "<<< session"
 
+  	respond_to do |format|
+			 format.html {  }
+		 end
   end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+APP_ID= '204060336365834' 
+APP_SECRET= 'b09cd7497b065848e232db1fe98365d7'
+SITE_URL = 'buddymeme.herokuapp.com'
