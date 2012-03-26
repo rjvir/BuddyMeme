f5c3c115e6c2dd3a846a2408599e363468541484
Chris Wendel <chriwend@umich.edu>
2012-03-26 00:37:47 -0400

    Testing after Delete

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BuddyMeme::Application.initialize!
-# config/environment.rb
-# in Rails::Initializer.run do |config|
-config.action_controller.allow_forgery_protection = false
-config.gem "koala"
