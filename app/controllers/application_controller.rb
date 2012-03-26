80be2001c9c5103a5477c4208260b76f7299dcec
Chris Wendel <chriwend@umich.edu>
2012-03-26 00:33:42 -0400

    Testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery
-  before_filter :parse_facebook_cookies
-  def parse_facebook_cookies
- 	@facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
-  end
+  protect_from_forgery
+
 end
