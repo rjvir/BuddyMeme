804844a8992ee84a296ff46fb8bdadad770a90ba
Chris Wendel <chriwend@umich.edu>
2012-03-26 00:31:11 -0400

    Testing Koala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
+  #protect_from_forgery
+  before_filter :parse_facebook_cookies
+  def parse_facebook_cookies
+ 	@facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BuddyMeme::Application.initialize!
+# config/environment.rb
+# in Rails::Initializer.run do |config|
+config.action_controller.allow_forgery_protection = false
+config.gem "koala"
