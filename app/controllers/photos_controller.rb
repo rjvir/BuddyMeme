8e9aab250c9e203a20573c0ac82433c3890690d3
Chris Wendel <chriwend@umich.edu>
2012-03-27 00:03:07 -0400

    OH

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
 	def show 
+		@api = Koala::Facebook::API.new(session[:access_token])
 		@identifier = params[:id];
 		profile_path = @api.get_picture(params[:id])
 	end
