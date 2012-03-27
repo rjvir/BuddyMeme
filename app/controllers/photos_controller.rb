c9cdedea78c85cfc66688e2f5c5a1b227943e9f6
Chris Wendel <chriwend@umich.edu>
2012-03-27 00:51:10 -0400

    nope

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,8 +2,8 @@ class PhotosController < ApplicationController
 	def show 
 		@api = Koala::Facebook::API.new(session[:access_token])
 		@identifier = params[:id];
-		@string = "SELECT aid, owner, name, object_id FROM album WHERE owner=@identifier";
-		@album_id = @api.fql_query("SELECT aid, owner, name, object_id FROM album WHERE owner=@identifier")
+		@string = 'SELECT aid, owner, name, object_id FROM album WHERE owner='@identifier'';
+		@album_id = @api.fql_query('SELECT aid, owner, name, object_id FROM album WHERE owner='@identifier'')
 		#@profile_path = @api.get_picture(params[:id])
 	end
 end
