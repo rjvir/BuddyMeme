81059d29269e8bb0520f82d4a7d7703b0b16e12b
Chris Wendel <chriwend@umich.edu>
2012-03-26 23:57:49 -0400

    Dont

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
 	def show 
 		@identifier = params[:id];
-		profile_path = @graph.get_picture(params[:id])
+		profile_path = @api.get_picture(params[:id])
 	end
 end
