e4be805099f6b3ed1731059af99f992b8a27062a
Chris Wendel <chriwend@umich.edu>
2012-03-27 00:43:32 -0400

    FuCKKF

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,8 @@
 class PhotosController < ApplicationController
 	def show 
 		@api = Koala::Facebook::API.new(session[:access_token])
-		@album_id = @api.fql_query("SELECT object_id FROM album WHERE name='Profile Pictures'")
-		#@identifier = params[:id];
+		@identifier = params[:id];
+		@album_id = @api.fql_query("SELECT aid, owner, name, object_id FROM album WHERE owner='@identifier'")
 		#@profile_path = @api.get_picture(params[:id])
 	end
 end
