4d5460417a052d20a8e914fe198872b99c02b138
Chris Wendel <chriwend@umich.edu>
2012-03-27 02:07:29 -0400

    plea

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,15 +5,15 @@ class PhotosController < ApplicationController
 		#@string = "SELECT aid FROM album WHERE owner='#{params[:id]}'"
 		@aids = @api.fql_query("SELECT aid, name FROM album WHERE owner='#{params[:id]}'")
 
-		@aids.each do |object|
-			if object["name"]=="Profile Pictures"
-				@profile_aid = object["aid"]
+		@aids.each do |photo_object|
+			if photo_object["name"]=="Profile Pictures"
+				@profile_aid = photo_object["aid"]
 				break
 			end
 		end
 		#@profile_path = @api.get_picture(params[:id])
 
 		@pics = @api.fql_query("SELECT src_big FROM photo WHERE aid='#{@profile_aid}'")
-		
+
 	end
 end
