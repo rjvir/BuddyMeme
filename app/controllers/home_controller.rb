2d6a91fcdfaa449ac5617e7ad247bbbd0f894057
Chris Wendel <chriwend@umich.edu>
2012-03-27 01:32:45 -0400

    n

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	protect_from_forgery
   def index
   		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
-		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"publish_stream,friends_photos") 	
+		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"friends_photos") 	
 		puts session.to_s + "<<< session"
 
   	respond_to do |format|
