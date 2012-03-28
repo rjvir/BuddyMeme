fb0cac3b111472fec9cedc79f839c9f33eafce4b
Chris Wendel <chriwend@umich.edu>
2012-03-28 01:23:08 -0400

    pl

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,8 +22,8 @@ class HomeController < ApplicationController
 		begin
 			#@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
 			@graph_data = @api.get_connections("me","friends")
-			@me = @graph.get_object("me");
-			@meUID = @me["id"];
+			me = @graph.get_object("me");
+			@meUID = me["id"];
 			@top_friends = @api.fql_multiquery(
 			:query1 => "SELECT actor_id, message, permalink, created_time, comments.count FROM stream WHERE source_id=me() LIMIT 0,150",
 			:query2 => "SELECT uid, name, pic_square, profile_url FROM user WHERE uid IN (SELECT actor_id from #query1) LIMIT 0,15"
