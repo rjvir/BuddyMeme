3939bceac6bc326482c7cb480c16820b8c752c39
Chris Wendel <chriwend@umich.edu>
2012-03-28 22:45:16 -0400

    da f

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,17 +3,28 @@ class PhotosController < ApplicationController
 		@api = Koala::Facebook::API.new(session[:access_token])
 		@identifier = params[:id];
 		#@string = "SELECT aid FROM album WHERE owner='#{params[:id]}'"
-		@aids = @api.fql_query("SELECT aid, name FROM album WHERE owner='#{params[:id]}'")
+		begin
+			@aids = @api.fql_query("SELECT aid, name FROM album WHERE owner='#{params[:id]}'")
+		rescue Exception=>ex
+			puts ex.message
+		end
 
+		begin
 		@aids.each do |photo_object|
 			if photo_object["name"]=="Profile Pictures"
 				@profile_aid = photo_object["aid"]
 				break
 			end
 		end
+		rescue Exception=>ex
+			puts ex.message
+		end 
 		#@profile_path = @api.get_picture(params[:id])
-
-		@pics = @api.fql_query("SELECT src_big FROM photo WHERE aid='#{@profile_aid}'")
+		begin
+			@pics = @api.fql_query("SELECT src_big FROM photo WHERE aid='#{@profile_aid}'")
+		rescue Exception=>ex
+			puts ex.message
+		end
 
 	end
 
