d53d6feb3f5c3638f2764a4ae62e7139e4814530
Chris Wendel <chriwend@umich.edu>
2012-03-29 00:52:21 -0400

    test

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,19 +3,19 @@ class PhotosController < ApplicationController
 		@api = Koala::Facebook::API.new(session[:access_token])
 		@identifier = params[:id];
 		#@string = "SELECT aid FROM album WHERE owner='#{params[:id]}'"
-		@profile_pics = @api.fql_multiquery(
-			:query1 => "SELECT aid, name FROM album WHERE owner='#{params[:id]}'",
-			:query2 => "SELECT src_big FROM photo WHERE aid IN (SELECT name from #query1)"
-			)
+		#@profile_pics = @api.fql_multiquery(
+		#	:query1 => "SELECT aid, name FROM album WHERE owner='#{params[:id]}'",
+		#	:query2 => "SELECT src_big FROM photo WHERE aid IN (SELECT name from #query1)"
+		#	)
 
-		#@aids = @api.fql_query("SELECT aid, name FROM album WHERE owner='#{params[:id]}'")
+		@aids = @api.fql_query("SELECT aid, name FROM album WHERE owner='#{params[:id]}'")
 
-		#@aids.each do |photo_object|
-		#	if photo_object["name"]=="Profile Pictures"
-		#		@profile_aid = photo_object["aid"]
-		#		break
-		#	end
-		#end
+		@aids.each do |photo_object|
+			if photo_object["name"]=="Profile Pictures"
+				@profile_aid = photo_object["aid"]
+				break
+			end
+		end
 		
 		#@pics = @api.fql_query("SELECT src_big FROM photo WHERE aid='#{@profile_aid}'")
 
