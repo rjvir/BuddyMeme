0b671a21d66618f68a21680731399d5d68790e32
Chris Wendel <chriwend@umich.edu>
2012-03-29 23:44:39 -0400

    fi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,8 @@ class HomeController < ApplicationController
 			@top_friends = @api.fql_multiquery(
 			:query1 => "SELECT actor_id FROM stream WHERE source_id=me() LIMIT 0,150",
 			:query2 => "SELECT uid, name, pic_square FROM user WHERE uid IN (SELECT actor_id from #query1) AND uid <> me() LIMIT 0,15",
-			:query3 => "SELECT uid2 FROM friend WHERE uid1 = me()"
+			:query3 => "SELECT uid2 FROM friend WHERE uid1 = me()",
+			:query4 => "SELECT name FROM user WHERE uid IN (SELECT uid2 from #query3")
 			)
 			#@test_query = @api.fql_query("SELECT pic_big FROM user WHERE uid='1200702'")
 		rescue Exception=>ex
