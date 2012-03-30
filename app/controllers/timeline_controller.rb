b11e5fb2fe19dcff4958f31bedd032d3225f0b4d
Chris Wendel <chriwend@umich.edu>
2012-03-30 01:01:19 -0400

    cc

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,8 +3,7 @@ class TimelineController < ApplicationController
 		@api = Koala::Facebook::API.new(session[:access_token])
 		begin
 			#@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
-			@filter_key_data = @api.fql_query("SELECT post_id, actor_id, target_id, message FROM stream WHERE filter_key in (SELECT filter_key FROM stream_filter WHERE uid=me() AND type='newsfeed') AND is_hidden = 0
-")
+			@filter_key_data = @api.fql_query("SELECT app_id from application WHERE display_name='Instagram'")
 
 			#@test_query = @api.fql_query("SELECT pic_big FROM user WHERE uid='1200702'")
 		rescue Exception=>ex
