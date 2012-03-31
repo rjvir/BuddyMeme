e49d93c3d64f15690f9e0498459ad37758bac518
rjvir <raj@rjvir.com>
2012-03-31 02:29:13 -0400

    bitch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
 	protect_from_forgery
   def index
-  		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
+  	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
 		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"friends_photos,publish_stream,read_stream") 	
 		puts session.to_s + "<<< session"
 
   	respond_to do |format|
 			 format.html {  }
-		 end
+		end
   end
 
   def callback
@@ -15,12 +15,11 @@ class HomeController < ApplicationController
   		# acknowledge code and get access token from FB
 		  session[:access_token] = session[:oauth].get_access_token(params[:code])
 		  redirect_to friends_path
-	end		
+	  end		
 
 		 # auth established, now do a graph call:
 
-
-	#end
+	end
 
 	def friends
 		@api = Koala::Facebook::API.new(session[:access_token])
@@ -36,8 +35,7 @@ class HomeController < ApplicationController
 		rescue Exception=>ex
 			puts ex.message
 		end
-
-  
+		
  		respond_to do |format|
 		 format.html {   }			 
 		end
