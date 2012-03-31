33761bb3c4f5d6912501b568cb099fe046fcf3ab
Chris Wendel <chriwend@umich.edu>
2012-03-31 00:14:26 -0400

    t

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -23,7 +23,7 @@ class PhotosController < ApplicationController
 
 	def tagged_photos
 		@api = Koala::Facebook::API.new(session[:access_token])
-		@tagged_photos = @api.fql_query("SELECT pid FROM photo_tag WHERE subject=#{params[:url]}")
+		@tagged_photos = @api.fql_query("SELECT pid FROM photo_tag WHERE subject=#{params[:id]}")
 	end
 
 	def make
