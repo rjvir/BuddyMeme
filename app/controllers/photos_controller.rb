944d3b1f3a3f3abca69820757c31c23cb5a6de3e
rjvir <raj@rjvir.com>
2012-04-01 00:58:16 -0400

    fucka

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -58,8 +58,8 @@ class PhotosController < ApplicationController
 #    @api.put_picture(@foo.remote_image_path,{}, album_id)
     @response = @api.put_picture(url, {:message => "Message"})
     #puts response
-    #id = response[:pid]
-    #args = {:x=>45, :y=>45, :tag_uid=>session[:id], :pid=>id}
-    #@api.rest_call('photos.addTag', args)
+    id = response[:id]
+    args = {:x=>45, :y=>45, :tag_uid=>session[:id], :pid=>id}
+    @api.rest_call('photos.addTag', args)
   end
 end
