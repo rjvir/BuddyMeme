2d9b9265de0f2118317f2ad6c0dcd10eb02a7cf0
rjvir <raj@rjvir.com>
2012-04-01 03:46:37 -0400

    fucka

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -59,7 +59,7 @@ class PhotosController < ApplicationController
     @response = @api.put_picture(url, {:message => "Message"})
     #puts response
     id = @response["post_id"]
-    args = {:x=>45, :y=>45,:to=>session[:id]}
+    args = {:x=>45, :y=>45,:to=>'573014549'}
     @api.put_object('100000503215765_133113', "tags", args)
 #    @api.rest_call('photos.addTag', args)
   end
