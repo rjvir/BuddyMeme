ad2c91d6460d1992ca422036b54cd9ef7d57addc
rjvir <raj@rjvir.com>
2012-04-01 00:16:32 -0400

    fucka

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -56,7 +56,9 @@ class PhotosController < ApplicationController
     url = "http://talktomindy.com/memes/#{params[:hash]}.png"
 	  @api = Koala::Facebook::API.new(session[:access_token])
 #    @api.put_picture(@foo.remote_image_path,{}, album_id)
-#    tags = {:x=>45, :y=>45, :to=>session[:id]}
-    @api.put_picture(url, {:message => "Message"})
+    tags = {:x=>45, :y=>45, :to=>session[:id]}
+    response = @api.put_picture(url, {:message => "Message"})
+    id = response["id"]
+    @api.put_tag(id, tags)
   end
 end
